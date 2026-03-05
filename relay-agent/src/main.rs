88475efb2c9c0e8ea7912de7f104c17a3fe3820f
Zhiyuan Zheng <zhizhen@microsoft.com>
2026-03-05 12:54:07 +0800

    fix(agent): enable default info logs for tracing_subscriber

--- relay-agent/src/main.rs
+++ relay-agent/src/main.rs
@@ -30,7 +30,11 @@ struct AgentConfig {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
+        )
+        .init();
     let args = Args::parse();
 
     // 1. Read config
